26f3c4330dadb7e7cd0ac911efcaa21c525ff5c6
chucklapress <charleslapress@gmail.com>
2016-07-17 12:16:39 -0400

    working on letter guesser to get program working using basic for loops

--- letter_guesser_new.py
+++ letter_guesser_new.py
@@ -1,13 +1,26 @@
 import random
-word_file = "/usr/share/dict/words"
-words = open(word_file).read().splitlines()
+file = "/usr/share/dict/words"
+words = open(file).read().splitlines()
 random_word = random.choice(words)
 list(random_word)
-secret_word = (list(random_word))
+secret_word = (random_word)
 print(secret_word)
-for character in secret_word:
-    print("_")
+for letter in secret_word:
+    print("-")
 
+guesses = "a,e,i,o,u"
+for letter in secret_word:
+    if letter in guesses:
+        print(letter),
+    else:
+        print("-")
+guess = input('guess a letter')
+guesses += guess
+for letter in secret_word:
+    if letter in guesses:
+        print(letter),
+    else:
+        print("_")
 
 
 
