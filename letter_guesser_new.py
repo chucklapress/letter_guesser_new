9c29eae1ad70a23fa5756a0c947e43b8041ff496
chucklapress <charleslapress@gmail.com>
2016-07-17 12:41:52 -0400

    added turns with count down and while to processes turns

--- letter_guesser_new.py
+++ letter_guesser_new.py
@@ -1,4 +1,26 @@
 import random
+#file = "/usr/share/dict/words"
+#words = open(file).read().splitlines()
+#random_word = random.choice(words)
+#list(random_word)
+#secret_word = (random_word)
+#print(secret_word)
+#for letter in secret_word:
+   # print("-")
+
+#guesses = "a,e,i,o,u"
+#for letter in secret_word:
+  #  if letter in guesses:
+    #    print(letter),
+ #   else:
+     #   print("-")
+#guess = input('guess a letter')
+#guesses += guess
+#for letter in secret_word:
+    #if letter in guesses:
+  #      print(letter),
+ #   else:
+ #       print("_")
 file = "/usr/share/dict/words"
 words = open(file).read().splitlines()
 random_word = random.choice(words)
@@ -9,18 +31,17 @@ for letter in secret_word:
     print("-")
 
 guesses = "a,e,i,o,u"
-for letter in secret_word:
-    if letter in guesses:
-        print(letter),
-    else:
-        print("-")
-guess = input('guess a letter')
-guesses += guess
-for letter in secret_word:
-    if letter in guesses:
-        print(letter),
-    else:
-        print("_")
+
+turns = 8
+while turns > 0 :
+    for letter in secret_word:
+        if letter in guesses:
+            print(letter),
+        else:
+            print("_")
+    guess = input('guess a letter')
+    guesses += guess
+    turns -= 1
 
 
 
