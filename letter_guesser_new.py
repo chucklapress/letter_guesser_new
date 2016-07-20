0eb6638bd91c8204608026a5606b72faf05a0867
chucklapress <charleslapress@gmail.com>
2016-07-20 12:07:24 -0400

    implimented changes suggested during code review

--- letter_guesser_new.py
+++ letter_guesser_new.py
@@ -24,33 +24,28 @@ import random
 file = "/usr/share/dict/words"
 words = open(file).read().splitlines()
 random_word = random.choice(words).lower()
-list(random_word)
-secret_word = (random_word)
+#list(random_word)
+secret_word = random_word
 print(secret_word)
-guesses = "a,e,i,o,u"
+guesses = "aeiou"
 #guesses = []
 turns = 8
 while turns > 0 :
     missed = 0
     for letter in secret_word:
         if letter in guesses:
-            print(letter),
+            print(letter,end = '')
         else:
-            print("_")
+            print("_", end = '')
             missed += 1
-            print(missed)
+            #print(missed)
     if missed == 0:
-        print('winner')
+        print('\n winner')
         break
-    guess = input('guess a letter')
+    guess = input('\n guess a letter')
     guesses += guess
     if guess not in secret_word:
         turns -= 1
         print('letter not in word')
         if turns == 0:
             print(secret_word)
-
-
-
-
-
